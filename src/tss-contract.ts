eaa471893e522ae9a008e3e163c59caa9d9bf3e8
danglongthanh45 <105013929+danglongthanh45@users.noreply.github.com>
2022-08-02 15:24:20 +0700

    Init

--- src/tss-contract.ts
+++ src/tss-contract.ts
@@ -1,83 +1,64 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  TSSContract,
+  Transfer as TransferEvent,
   AddBlacklistEvent,
-  Approval,
-  ApprovalForAll,
   RemoveBlacklistEvent,
-  RoleAdminChanged,
-  RoleGranted,
-  RoleRevoked,
-  Transfer
-} from "../generated/TSSContract/TSSContract"
-import { ExampleEntity } from "../generated/schema"
+  TSSContract as TSSContract
+ } from "../generated/TSSContract/TSSContract"
 
-export function handleAddBlacklistEvent(event: AddBlacklistEvent): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+import {
+  Token, User
+ } from '../generated/schema'
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+export function handleTransfer(event: TransferEvent): void {
+  let token = Token.load(event.params.tokenId.toString());
+  if (!token) {
+    token = new Token(event.params.tokenId.toString());
+    //token.creator = event.params.to.toHexString();
+    token.creator = event.transaction.from.toHexString();
+    token.tokenID = event.params.tokenId;
+    token.createdAtTimestamp = event.block.timestamp; 
+    // let tssContract = TSSContract.bind(event.address);
+    // token.metadataURI = tssContract.tokenURI(event.params.tokenId);
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.addresses = event.params.addresses
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DEFAULT_ADMIN_ROLE(...)
-  // - contract.MINTER_ROLE(...)
-  // - contract.balanceOf(...)
-  // - contract.getApproved(...)
-  // - contract.getRoleAdmin(...)
-  // - contract.hasRole(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.isBlacklist(...)
-  // - contract.name(...)
-  // - contract.ownerOf(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenByIndex(...)
-  // - contract.tokenOfOwnerByIndex(...)
-  // - contract.tokenURI(...)
-  // - contract.totalSupply(...)
+  token.owner = event.params.to.toHexString();
+  token.save();
+ 
+  let user = User.load(event.params.to.toHexString());
+  if (!user) {
+    user = new User(event.params.to.toHexString());
+    user.isBlacklist = false;
+    user.save();
+  }
 }
 
-export function handleApproval(event: Approval): void {}
+export function handleAddBlacklist(event: AddBlacklistEvent): void { 
+  let listAddressInput = event.params.addresses;
+  listAddressInput.forEach(element => {
+    let user = User.load(element.toHexString());
+    if (user) {
+      user.isBlacklist = true;
+      user.save();
+    }
+    else {
+      user = new User(element.toHexString());
+      user.isBlacklist = true;
+      user.save();
+    }
+  });
+}
 
-export function handleApprovalForAll(event: ApprovalForAll): void {}
-
-export function handleRemoveBlacklistEvent(event: RemoveBlacklistEvent): void {}
-
-export function handleRoleAdminChanged(event: RoleAdminChanged): void {}
-
-export function handleRoleGranted(event: RoleGranted): void {}
-
-export function handleRoleRevoked(event: RoleRevoked): void {}
-
-export function handleTransfer(event: Transfer): void {}
+export function handleRemoveBlacklist(event: RemoveBlacklistEvent): void { 
+  let listAddressInput = event.params.addresses;
+  listAddressInput.forEach(element => {
+    let user = User.load(element.toHexString());
+    if (user) {
+      user.isBlacklist = false;
+      user.save();
+    }
+    else {
+      user = new User(element.toHexString());
+      user.isBlacklist = false;
+      user.save();
+    }
+  });
+}
